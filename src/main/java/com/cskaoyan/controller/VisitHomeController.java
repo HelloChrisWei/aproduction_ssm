95d6dc1801423534c4cbcbe9148faed47ae00ea2
zck2333 <1213779298@qq.com>
2019-06-27 11:21:13 +0800

    commit VisitHomeController by zck

--- src/main/java/com/cskaoyan/controller/VisitHomeController.java
+++ src/main/java/com/cskaoyan/controller/VisitHomeController.java
@@ -1,10 +1,13 @@
 package com.cskaoyan.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class VisitHome {
+@Controller
+public class VisitHomeController {
+
     @RequestMapping("/")
-    public String ia(){
+    public String visitHome() {
         return "home";
     }
 }
