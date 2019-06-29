cdd3462efb2e9c89947858f2d34c169e0e451693
zck2333 <1213779298@qq.com>
2019-06-29 11:55:31 +0800

    commit update something by zck

--- src/main/java/com/cskaoyan/controller/VisitHomeController.java
+++ src/main/java/com/cskaoyan/controller/VisitHomeController.java
@@ -1,13 +1,13 @@
-package com.cskaoyan.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class VisitHomeController {
-
-    @RequestMapping("/")
-    public String visitHome() {
-        return "home";
-    }
-}
+//package com.cskaoyan.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class VisitHomeController {
+//
+//    @RequestMapping("/")
+//    public String visitHome() {
+//        return "home";
+//    }
+//}
